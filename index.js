2ec072fbfdaf5b9e152809019790b8ed031d4ba5
judy_her <judith85dm@ucla.edu>
2024-01-12 13:35:11 -0800

    added prompts for color and shapes, added write file function

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const { writeFile } = require('fs').promises;
 const questions = () => {
   return inquirer.prompt([
     {
-      name: 'shape',
+      name: 'text',
       type: 'input',
-      message: 'What shape would you like? Choose between',
+      message: 'What three characters should be in your svg?',
+    },
+    {
+      name: 'color',
+      type: 'input',
+      message: 'What color will your svg be? Type color name or hexadecimal: ',
+    },
+    {
+      name: 'shape',
+      type: 'list',
+      message: 'What shape will your svg be? ',
+      choices: ['cirlce', 'triangle', 'square'],
     },
   ]);
 };
+
+//write file
+const init = () => {
+  questions()
+    .then((answers) => writeFile('logo.svg', generateSVG(answers)))
+    .then(() => console.log('SVG succesfully created'))
+    .catch((err) => console.log(err));
+};
+
+init();
