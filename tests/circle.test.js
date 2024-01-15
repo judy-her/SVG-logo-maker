c778ef3d649ef7f079371f6e4167ee1bd76527c3
judy_her <judith85dm@ucla.edu>
2024-01-15 14:23:42 -0800

    added circle test and fixed render method

---
+++ tests/circle.test.js
@@ -0,0 +1,14 @@
+const Circle = require('../lib/circle.js');
+
+//test suite to render circle shape
+
+describe('Circle', () => {
+  describe('render circle', () => {
+    it('Should render a circle', () => {
+      const radius = 10;
+      const newCircle = new Circle(radius);
+      const result = `<circle cx="${radius}" cy="${radius}" r="${radius}" fill="[COLOR]" />`;
+      expect(newCircle.render()).toEqual(result);
+    });
+  });
+});
--- tests/svgElement.test.js
+++ tests/svgElement.test.js
@@ -3,11 +3,11 @@ const SVGElement = require('../lib/svgElement.js');
 //testing suite for SVGElement to throw error when renderInner() is called directly
 
 describe('SVGElement', () => {
-  describe('renderInner', () => {
-    it('Should throw error message when calling directly', () => {
+  describe('render', () => {
+    it('Should throw error message when calling render directly', () => {
       const newSVGElem = new SVGElement();
-      const err = new Error('Child class must implement renderInner() method.');
-      expect(newSVGElem.renderInner).toThrow(err);
+      const err = new Error('Child class must implement render() method.');
+      expect(newSVGElem.render).toThrow(err);
     });
   });
 });
