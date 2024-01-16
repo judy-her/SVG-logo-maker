f4130ed55f2b9bf25bc926245a547322c9be01dc
judy_her <judith85dm@ucla.edu>
2024-01-15 19:50:17 -0800

    added methods to svgElements and added tests that passed for those methods

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -1,13 +1,12 @@
 const Circle = require('../lib/circle.js');
 
 //test suite to render circle shape
-
 describe('Circle', () => {
   describe('render circle', () => {
     it('Should render a circle', () => {
       const radius = 10;
       const newCircle = new Circle(radius);
-      const result = `<circle cx="${radius}" cy="${radius}" r="${radius}" fill="[COLOR]" />`;
+      const result = `<circle cx="${radius}" cy="${radius}" r="${radius}"/>`;
       expect(newCircle.render()).toEqual(result);
     });
   });
--- tests/svgElement.test.js
+++ tests/svgElement.test.js
@@ -1,7 +1,7 @@
 const SVGElement = require('../lib/svgElement.js');
 
 //testing suite for SVGElement to throw error when renderInner() is called directly
-
+//NOTE Passed
 describe('SVGElement', () => {
   describe('render', () => {
     it('Should throw error message when calling render directly', () => {
@@ -11,3 +11,41 @@ describe('SVGElement', () => {
     });
   });
 });
+//testing width and height
+//NOTE Passed
+// describe('SVGElement', () => {
+//   describe('render width and height', () => {
+//     it('should render any given width and height', () => {
+//       const width = 300;
+//       const height = 200;
+//       const newSize = new SVGElement(width, height);
+//       const result = `<svg width="${width}" height="${height}"></svg>`;
+//       expect(newSize.renderSize()).toEqual(result);
+//     });
+//   });
+// });
+
+// testing color
+//NOTE Passed
+describe('SVGElement', () => {
+  describe('color', () => {
+    it('should render color in fill attribute', () => {
+      const color = 'blue';
+      const newColor = new SVGElement(300, 200);
+      newColor.setColor(color);
+      const result = `<svg width="300" height="200" fill="${color}"></svg>`;
+      expect(newColor.renderSize()).toEqual(result);
+    });
+  });
+});
+
+// describe('SVGElement', () => {
+//   describe('renderInner', () => {
+//     it('Should render fill color when color is set', () => {
+//       const shape = new SVGElement(100, 100);
+//       shape.setColor('blue');
+//       const result = `<svg width="100" height="100" fill="blue"></svg>`;
+//       expect(shape.renderInner()).toEqual(result);
+//     });
+//   });
+// });
